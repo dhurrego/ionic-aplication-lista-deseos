36b446645453a97764f709fd9387fa5396198578
Deivid Urrego <deivid_199615@hotmail.com>
2021-01-11 19:50:46 -0500

    Se crear los metodos guardarStorage y cargarStorage en el servicio deseos.service para almacenar los datos en el localstorage del navegador

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -9,10 +9,28 @@ export class DeseosService {
 
   public listas: Lista[] = [];
 
-  constructor() {}	
+  constructor() {
+
+  	this.cargarStorage();
+
+  }	
 
   crearLista( titulo: string ) {
   	const nuevaLista = new Lista( titulo );
   	this.listas.push( nuevaLista );
+  	this.guardarStorage();
+  }
+
+  guardarStorage(){
+  	localStorage.setItem('data', JSON.stringify(this.listas));
+  }
+
+  cargarStorage(){
+  	if(localStorage.getItem('data')){
+  		this.listas = JSON.parse( localStorage.getItem('data') );	
+  	}else{
+  		this.listas = [];
+  	}
+  	
   }
 }
